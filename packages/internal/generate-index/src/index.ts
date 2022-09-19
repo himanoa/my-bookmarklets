3975729de59e8377c4bf55f8dfb96e700df94c6b
himanoa <matsunoappy@gmail.com>
2022-09-20 05:37:34 +0900

    Remove description

--- packages/internal/generate-index/src/index.ts
+++ packages/internal/generate-index/src/index.ts
@@ -27,7 +27,6 @@ const outputMarkdown = `
 
 1. chrome://bookmarks/ にアクセスしてブックマークマネージャを開く
 1. 右上のメニューより 新しいブックマークを追加 を選択する
-1. URL として下記の URL (javascript: からすべて) を入力する。名前はわかりやすいもので構わない (例: UUID コピーなど)
 
 ${targetDirectories.reduce((acc, value) => {
   return acc + generateSection(value)
